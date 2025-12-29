3bf40f69754e7b26e57f55523e9719c7a9339402
kushalvora <kushalvora@gmail.com>
2025-12-28 20:58:44 -0800

    Initial Commit

---
+++ SpeedMenuBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeedMenuBar
+//
+//  Created by Kushal Vora on 12/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeedMenuBar/SpeedMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedMenuBarApp.swift
+//  SpeedMenuBar
+//
+//  Created by Kushal Vora on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeedMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
